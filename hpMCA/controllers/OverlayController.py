ed663e3cd19782eb731f8106ee0ba6e86211b54c
Ross Hrubiak <hrubiak@gmail.com>
2019-10-16 10:10:01 -0500

    Update OverlayController.py

--- hpMCA/controllers/OverlayController.py
+++ hpMCA/controllers/OverlayController.py
@@ -35,7 +35,7 @@ class OverlayController(object):
     the corresponding overlay data in the Pattern Model.
     """
 
-    def __init__(self, mcc, plot_controller, pltWidget):
+    def __init__(self, mcc, plot_controller, hpMcaWidget):
         """
         :param widget: Reference to IntegrationWidget object
         :param pattern_model: Reference to PatternModel object
@@ -47,7 +47,7 @@ class OverlayController(object):
         self.overlay_widget = OverlayWidget()
         self.model = mcc
         self.overlay_model = OverlayModel()
-        self.pattern_widget = pltWidget
+        self.pattern_widget = hpMcaWidget.pg
         self.plotController = plot_controller
         
         
