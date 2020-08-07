9001708acd484b5291adb27f39d577e04a54a3d0
Ross <hrubiak@gmail.com>
2020-08-07 15:23:15 -0500

    pyeqt

---
+++ pyeqt/pvServer.py
@@ -0,0 +1,42 @@
+
+
+import  time, logging, os, struct, sys, copy
+
+from pyqtgraph.Qt import QtCore
+from PyQt5.QtCore import QThread, pyqtSignal, QObject
+import queue 
+from functools import partial
+import json
+from epics import PV, caget, caput
+from pyeqt.epicsMonitor import epicsMonitor
+
+class pvServer():
+    pvs = {}
+    pv_monitors = {}
+    
+   
+    def set_pv(self, pv_name, pv):
+        self.pvs[pv_name] = pv
+        monitor = epicsMonitor(pv, autostart=True)  
+        self.pv_monitors[pv_name] = monitor
+
+    def get_pv(self, pv_name):
+        #print(pv_name)
+        if pv_name in self.pvs:
+            pv = self.pvs[pv_name]
+        else:
+            pv = PV(pv_name)
+            self.set_pv(pv_name, pv)
+        
+        return pv
+
+    def get_pv_monitor(self, pv_name):
+        if pv_name in self.pvs and pv_name in self.pv_monitors:
+            monitor = self.pv_monitors[pv_name]
+        else:
+            pv = PV(pv_name)
+            self.set_pv(pv_name, pv)
+            monitor = self.pv_monitors[pv_name]
+        return monitor
+
+
