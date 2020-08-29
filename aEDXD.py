e11fc03ca1101a505d45fa4717f42b47be055344
Ross-home-mac <hrubiak@gmail.com>
2020-08-28 22:15:18 -0500

    Update aEDXD.py

--- aEDXD.py
+++ aEDXD.py
@@ -62,9 +62,9 @@
 #       11. Save file-use tag
 #       12. Periodic table atom picker
 #       13. TODO sequence file loading 
-#       14. TODO Integrate scanning data acquisition and data display
-#       15. TODO better peak cutting based on background function, use LV bg function
-#       16. TODO Hdf5 compatible, save datafiles in projext files, save parameters in exported files?
+#       14. TODO Hdf5 compatible, save datafiles in projext files, save parameters in exported files?
+#       15. TODO Integrate scanning data acquisition and data display
+#       16. TODO better peak cutting based on background function, use LV bg function
 #       17. TODO improve autonaming of exported files, export all option
 #       18. TODO Allow different E range for different segments 
 #       19. TODO Optimal parameters not found when Emax 69.5, prorgram crashes
