4e3b1e7d8a78f6dca375316b31af2c028b99f535
Ross Hrubiak <hrubiak@gmail.com>
2022-12-10 10:50:41 -0600

    Update __init__.py

--- hpm/widgets/plot_widgets/__init__.py
+++ hpm/widgets/plot_widgets/__init__.py
@@ -18,5 +18,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .ImgWidget import MaskImgWidget, ImgWidget
+from .ImgWidget import MaskImgWidget, ImgWidget2
 #from .PatternWidget import PatternWidget 
